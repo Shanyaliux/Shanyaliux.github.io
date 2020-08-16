607396e025d638b5ec3fe93b713e97eca30137fa
Shanyaliux <445502929@qq.com>
2020-08-16 11:51:00 +0800

    Site updated: 2020-08-16 11:50:52

---
+++ 404/style/script.js
@@ -0,0 +1,105 @@
+gsap.set("svg", { visibility: "visible" });
+gsap.to("#headStripe", {
+  y: 0.5,
+  rotation: 1,
+  yoyo: true,
+  repeat: -1,
+  ease: "sine.inOut",
+  duration: 1 });
+
+gsap.to("#spaceman", {
+  y: 0.5,
+  rotation: 1,
+  yoyo: true,
+  repeat: -1,
+  ease: "sine.inOut",
+  duration: 1 });
+
+gsap.to("#craterSmall", {
+  x: -3,
+  yoyo: true,
+  repeat: -1,
+  duration: 1,
+  ease: "sine.inOut" });
+
+gsap.to("#craterBig", {
+  x: 3,
+  yoyo: true,
+  repeat: -1,
+  duration: 1,
+  ease: "sine.inOut" });
+
+gsap.to("#planet", {
+  rotation: -2,
+  yoyo: true,
+  repeat: -1,
+  duration: 1,
+  ease: "sine.inOut",
+  transformOrigin: "50% 50%" });
+
+
+gsap.to("#starsBig g", {
+  rotation: "random(-30,30)",
+  transformOrigin: "50% 50%",
+  yoyo: true,
+  repeat: -1,
+  ease: "sine.inOut" });
+
+gsap.fromTo(
+"#starsSmall g",
+{ scale: 0, transformOrigin: "50% 50%" },
+{ scale: 1, transformOrigin: "50% 50%", yoyo: true, repeat: -1, stagger: 0.1 });
+
+gsap.to("#circlesSmall circle", {
+  y: -4,
+  yoyo: true,
+  duration: 1,
+  ease: "sine.inOut",
+  repeat: -1 });
+
+gsap.to("#circlesBig circle", {
+  y: -2,
+  yoyo: true,
+  duration: 1,
+  ease: "sine.inOut",
+  repeat: -1 });
+
+
+gsap.set("#glassShine", { x: -68 });
+
+gsap.to("#glassShine", {
+  x: 80,
+  duration: 2,
+  rotation: -30,
+  ease: "expo.inOut",
+  transformOrigin: "50% 50%",
+  repeat: -1,
+  repeatDelay: 8,
+  delay: 2 });
+
+
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('nav');
+
+burger.addEventListener('click', e =&gt; {
+  burger.dataset.state === 'closed' ? burger.dataset.state = "open" : burger.dataset.state = "closed";
+  nav.dataset.state === "closed" ? nav.dataset.state = "open" : nav.dataset.state = "closed";
+});<script>
+        document.querySelectorAll('.github-emoji')
+          .forEach(el => {
+            if (!el.dataset.src) { return; }
+            const img = document.createElement('img');
+            img.style = 'display:none !important;';
+            img.src = el.dataset.src;
+            img.addEventListener('error', () => {
+              img.remove();
+              el.style.color = 'inherit';
+              el.style.backgroundImage = 'none';
+              el.style.background = 'none';
+            });
+            img.addEventListener('load', () => {
+              img.remove();
+            });
+            document.body.appendChild(img);
+          });
+      </script>
