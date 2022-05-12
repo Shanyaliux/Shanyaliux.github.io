d95cfc5370c03acb247befacd4607e2028b45316
Shanyaliux <445502929@qq.com>
2022-05-12 11:00:31 +0800

    push

--- deploy.sh
+++ deploy.sh
@@ -6,21 +6,9 @@ set -e
 # deploy to github pages
 echo 'shanyaliux.cn' > CNAME
 
-
+python ./rtfd/getIndex.py 
 
 git add .
 git commit -m "push"
 git push
 
-# deploy to coding pages
-# echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
-# echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
-
-# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-#   codingUrl=git@e.coding.net:xgy/xgy.git
-# else
-#   codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
-# fi
-# git add -A
-# git commit -m "${msg}"
-# git push -f $codingUrl master # 推送到coding
