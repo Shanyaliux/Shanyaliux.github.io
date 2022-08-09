3390a3bf22fa2bf1c346eb4d2e4e6d73b53faa3b
Shanyaliux <445502929@qq.com>
2022-08-10 03:45:40 +0800

    push

--- deploy.bat
+++ deploy.bat
@@ -3,6 +3,7 @@ echo 'shanyaliux.cn' > CNAME
 
 python utils/autoFrontmatter.py
 
+ssh root@shanyaliux.cn chattr -i /www/wwwroot/blog/docs/.vuepress/dist/.user.ini
 ssh root@shanyaliux.cn rm -rf /www/wwwroot/blog/*
 scp -r -C ./* root@shanyaliux.cn:/www/wwwroot/blog
 ssh root@shanyaliux.cn bash deployBlog.sh
