d9f688bb11b806b6bcd15789136f8342b35a8d00
Shanyaliux <445502929@qq.com>
2022-08-10 10:40:10 +0800

    push

--- deploy.bat
+++ deploy.bat
@@ -3,11 +3,6 @@ echo 'shanyaliux.cn' > CNAME
 
 python utils/autoFrontmatter.py
 
-@REM ssh root@shanyaliux.cn chattr -i /www/wwwroot/blog/docs/.vuepress/dist/.user.ini
-@REM ssh root@shanyaliux.cn rm -rf /www/wwwroot/blog/*
-@REM scp -r -C ./* root@shanyaliux.cn:/www/wwwroot/blog
-@REM ssh root@shanyaliux.cn bash deployBlog.sh
-
 git add .
 git commit -m "push"
 git push
--- deploy.ps1
+++ deploy.ps1
@@ -4,10 +4,6 @@ echo 'shanyaliux.cn' > CNAME
 
 python utils/autoFrontmatter.py
 
-ssh root@shanyaliux.cn rm -rf /root/blog/*
-scp -r -C ./* root@shanyaliux.cn:/root/blog
-ssh root@shanyaliux.cn bash deployBlog.sh
-
 git add .
 git commit -m "push"
 git push
